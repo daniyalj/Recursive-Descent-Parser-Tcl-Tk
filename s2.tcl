e300c339d6241b821b4218bb6f8ac18fe335fd5f
dsjili <iliaguievdavid@gmail.com>
2016-07-25 10:42:33 -0400

    Update s2.tcl

--- s2.tcl
+++ s2.tcl
@@ -1,1 +1,8 @@
-
+#
+#	David Iliaguiev, 210479830, davidili
+#	Daniyal Javed, 212654570, cse31034
+#	Alesya Orina, 212290987, cse23221
+#
+#
+#	Implementation for a state diagram for the language of EBNF
+#
