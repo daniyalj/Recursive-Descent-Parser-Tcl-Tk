a10aa06a6405a836e2db89fefd440f8e5721cb70
dsjili <iliaguievdavid@gmail.com>
2016-07-25 10:43:07 -0400

    Update s3.tcl

--- s3.tcl
+++ s3.tcl
@@ -1,3 +1,12 @@
+#
+# David Iliaguiev, 210479830, davidili
+# Daniyal Javed, 212654570, cse31034
+# Alesya Orina, 212290987, cse23221
+#
+#
+# Implementation for a Recursive Descent Parser of the language of EBNF
+#
+
  namespace eval ::RDP {
     variable rdp_type
     variable rule_code
